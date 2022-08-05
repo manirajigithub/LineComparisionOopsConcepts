743decc7e49d51a23dc6b8360c560c6900bbe9c9
mani1 <mani1@NANI143>
2022-08-05 16:01:09 +0530

    Added UC1 Length Of Line Program

---
+++ UC1_Length.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+public class UC1_Length
+{
+
+    int X1, X2, Y1, Y2;
+    double Length;
+    public void Lengthcal()
+    {
+        Console.WriteLine("Enter value for  coordinate x1 y1 x2 y2");
+        X1 = Convert.ToInt32(Console.ReadLine());
+        X2 = Convert.ToInt32(Console.ReadLine());
+        Y1 = Convert.ToInt32(Console.ReadLine());
+        Y2 = Convert.ToInt32(Console.ReadLine());
+        Length = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+        double len = Math.Round(Length, 2);
+        Console.Write("Calculate The Length Of Line");
+    }
+
+}
