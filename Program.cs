b69cdab46c8981045ea7f75314d8893fbf2e44d7
mani1 <mani1@NANI143>
2022-08-05 16:24:29 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace LineComparisionUC4
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate the comparing the length");
+            UC4_EqualityOfPonitsAndLines lc = new UC4_EqualityOfPonitsAndLines();
+        }
+    }
+}
