c754d85fa58d76d25808159f0fc97c7a31a6bb7a
mani1 <mani1@NANI143>
2022-08-05 15:58:16 +0530

    Added program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LineComparisionUC1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate The Length Of Line");
+            UC1_Length lc = new UC1_Length();
+            lc.Lengthcal();
+        }
+    }
+}
+
