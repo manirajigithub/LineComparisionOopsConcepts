9fa28f779d7bd8509039f275f66fea1289598717
mani1 <mani1@NANI143>
2022-08-05 16:13:52 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparisionCompareUC3
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate the comparing the length");
+            UC3_CompareLength lc = new UC3_CompareLength();
+        }
+    }
+}
